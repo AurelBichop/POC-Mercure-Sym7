eee11f616c5fdf299a3e3b4db5fad310d0717a48
AurelBichop <linux.aurelien@gmail.com>
2025-10-27 22:06:59 +0100

    End of POC

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Factory\ConversationFactory;
 use App\Repository\ConversationRepository;
+use App\Service\TopicService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\Authorization;
+use Symfony\Component\Mercure\Discovery;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
+
 
 /**
  * @method User|null getUser()
@@ -16,24 +20,33 @@ use Symfony\Component\Security\Core\User\UserInterface;
 final class ConversationController extends AbstractController
 {
     public function __construct(
+        private readonly Authorization $authorization,
+        private readonly Discovery $discovery,
         private readonly ConversationRepository $conversationRepository,
         private readonly ConversationFactory $factory,
+        private readonly TopicService $topicService,
     ) {
     }
 
     #[Route('/conversation/users/{recipient}', name: 'conversation.show')]
-    public function index(?User $recipient): Response
+    public function index(?User $recipient, Request $request): Response
     {
         $sender = $this->getUser();
 
-        $conversations = $this->conversationRepository->findByUsers($sender, $recipient);
+        $conversation = $this->conversationRepository->findByUsers($sender, $recipient);
 
-        if (!$conversations) {
-            $conversations = $this->factory->create($sender, $recipient);
+        if (!$conversation) {
+            $conversation = $this->factory->create($sender, $recipient);
         }
 
+        $topic = $this->topicService->getTopicUrl($conversation);
+
+        $this->discovery->addLink($request);
+        $this->authorization->setCookie($request, [$topic]);
+
         return $this->render('conversation/show.html.twig', [
-            'conversation' => $conversations,
+            'conversation' => $conversation,
+            'topic' => $this->topicService->getTopicUrl($conversation)
         ]);
     }
 }
--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -6,9 +6,12 @@ use App\DTO\CreateMessage;
 use App\Entity\User;
 use App\Factory\MessageFactory;
 use App\Repository\ConversationRepository;
+use App\Service\TopicService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Attribute\Route;
 
 
@@ -18,8 +21,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MessageController extends AbstractController
 {
     public function __construct(
-        private ConversationRepository $conversationRepository,
+        private readonly ConversationRepository $conversationRepository,
         private readonly MessageFactory $factory,
+        private readonly HubInterface $hub,
+        private readonly TopicService $topicService,
     ) {
     }
 
@@ -33,6 +38,19 @@ final class MessageController extends AbstractController
             author: $this->getUser(),
             content: $payload->content
         );
+
+        $data = [
+            'authorId' => $message->getAuthor()->getId(),
+            'content' => $message->getContent(),
+        ];
+
+        $update = new Update(
+            topics: $this->topicService->getTopicUrl($conversation),
+            data: json_encode($data),
+            private: true,
+        );
+
+        $this->hub->publish($update);
 
         return new Response('', Response::HTTP_CREATED);
     }
